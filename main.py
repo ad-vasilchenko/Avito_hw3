978df99adf23d8b9c4e31c62c425a257676a8aa0
Andrey Vasichenko <ad.vasilchenko@yandex.ru>
2020-12-15 17:36:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,41 @@
 import json
 
 class ColorizeMixin:
-    color = 33
     
+    def __str__(self):
+        return f" \033[1;30;{self.repr_color_code}m{self.__repr__()}"
 
 class Advert(ColorizeMixin):
-    def __init__ (self, d):
-        if 'price' in d and d['price'] < 0:
-            raise ValueError ('must be >= 0')
-            
+    def __init__ (self, d):          
+        self.repr_color_code = 32
         for key in d:
             self.__setattr__(key,d[key])
             
-    def __setattr__(self, key, value):
-        
-        key_private = '_' + key
-        if type(value) == dict:
-            self.__dict__[key_private] = Advert(value)
-        else:
-            self.__dict__[key_private] = value
-            
-    def __getattr__ (self, key):
-        key_private = '_' + key
-        if hasattr(self, key_private):
-            return self.__dict__[key_private]
-        else:
-            raise ValueError ('no such value')
-            
-            
     @property
     def price(self):
-        if '_price' in self.__dict__:
-            return self._price
-        else:
-            return 0
-
+        return self.__dict__.get('price', 0)
         
     @price.setter
     def price(self, p):
         if p >= 0:
-            self._price = p
+            self.price = p
         else:
             raise ValueError ('must be >= 0')
             
-    def __str__(self):
-        return f" \033[1;30;{super().color}m  {self.title} | {self.price} "
-
+    def __setattr__(self, key, value):
+        if isinstance(value, dict):
+            self.__dict__[key] = Advert(value)
+        else:
+            self.__dict__[key] = value
+            
+    def __getattr__ (self, key):
+        if hasattr(self, key):
+            return self.__dict__[key]
+        else:
+            raise ValueError ('no such value')
+            
     def __repr__(self):
-        return f" \033[1;30;{super().color}m  {self.title} | {self.price} "
+        return f"{self.title} | {self.price}"
 
 
 if __name__ == "__main__":
@@ -65,4 +53,6 @@ if __name__ == "__main__":
     print (data)
     print(a.location.address)
     print(a.price)
+    print(a)
+    a.repr_color_code = 33
     print(a)
